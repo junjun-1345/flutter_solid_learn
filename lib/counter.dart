40a20fa3a613914115e94e66a3c978bdb56f99e9
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2025-10-25 17:31:24 +0900

    feat: 状態管理

---
+++ lib/counter.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_solidart/flutter_solidart.dart';
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  final counter = Signal<int>(0, name: 'counter');
+  late final doubleCounter = Computed<int>(
+    () => counter.value * 2,
+    name: 'doubleCounter',
+  );
+
+  @override
+  void dispose() {
+    counter.dispose();
+    doubleCounter.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SignalBuilder(
+          builder: (context, child) {
+            return Text(
+              'Counter is ${counter.value}, doubleCounter is ${doubleCounter.value}',
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => counter.value++,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ source/counter.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:solid_annotations/solid_annotations.dart';
+
+class Counter extends StatelessWidget {
+  Counter({super.key});
+
+  @SolidState()
+  int counter = 0;
+
+  @SolidState()
+  int get doubleCounter => counter * 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Counter is $counter, doubleCounter is $doubleCounter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => counter++,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
